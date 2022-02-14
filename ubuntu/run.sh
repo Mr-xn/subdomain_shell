8a6d5c4b3a62cf50ffaf6fb8132f65173db528f9
东方有鱼名为咸 <Mr-xn@users.noreply.github.com>
2022-02-14 20:17:54 +0800

    Update run.sh

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -15,6 +15,8 @@ if [ ! -d ${domain} ];then
     fi
 # 子域名文件
 sub_file=${domain}'/'${domain}'_sub.txt'
+# 验证结果文件
+sub_file_ok=${domain}'/'${domain}'_sub_ok.txt'
 # 各个域名对应标题、状态吗等信息文件
 title_file=${domain}'/'${domain}'_title.txt'
 
@@ -22,26 +24,28 @@ title_file=${domain}'/'${domain}'_title.txt'
 # 由于目前 httpx 输出文件如果不加 -no-color 参数，保存结果文件会包含颜色代码,故使用perl替换了一下后再保存
 
 sub="./subfinder -d ${domain} -o ${sub_file} -silent"
-ksubdomain='sudo ./ksubdomain -verify -skip-wild -silent'
-httpx="./httpx -title -content-length -status-code -o tmp.txt &&cat tmp.txt|perl -pe 's/\e\[[0-9;]*m//g'|tee ${title_file}>>/dev/null 2>&1|rm tmp.txt"
+ksubdomain="sudo ./ksubdomain v -b 20M -f ${sub_file} --od -o ${sub_file_ok}"
+httpx="./httpx -t 30 -rl 60 -fr -maxr 3 -retries 3 -timeout 3 -ec -sc -cl -title -l ${sub_file_ok} -o tmp.txt &&cat tmp.txt|perl -pe 's/\e\[[0-9;]*m//g'|tee ${title_file}>>/dev/null 2>&1|rm tmp.txt"
+# -proxy http://127.0.0.1:8080 
 
 # 如果子域名为空，删掉结果
 del_null_sub(){
 if [ -s ${title_file} ];then
-    sleep 1s
-    printf "[+] 获取到域名 <<${domain}'s>> 的子域名! \n[+] 保存在 <<${domain}>> 文件夹 \n"
-    printf "=>>= 共 `wc -l ${title_file}|awk '{print $1}'` 条有效域名 \n"
+    sleep 1
+    printf "[+] 获取域名 <<${domain}>> 的子域名完成! \n[+] 保存在 <<${domain}>> 文件夹 \n"
+    printf "=>>= 共 `wc -l ${sub_file}|awk '{print $1}'` 条子域名 \n"
+    printf "=>>= 共 `wc -l ${sub_file_ok}|awk '{print $1}'` 条有效子域名 \n"
+    printf "=>>= 共 `wc -l ${title_file}|awk '{print $1}'` 条可正常访问域名 \n"
 else
-    printf "[+] 没有获取到域名 <<${domain}'s>> 的任何有效域名！ \n"
+    printf "[+] 没有获取到域名 <<${domain}>> 的任何有效域名！ \n"
     rm -rf ${domain}
     exit 1
 fi
 }
 
-find_subdomain(){
+over(){
 # 联合执行命令打印保存结果
 if [ $? -eq 0 ];then
-    sleep 1s
     del_null_sub
     printf "=>>= 结束时间:`date` \n"
 else
@@ -54,14 +58,20 @@ fi
 printf "=>>= 作者:Mrxn \n"
 printf "=>>= 脚本更新/反馈地址: \n"
 printf "=>>= https://github.com/Mr-xn/subdomain_shell \n"
+printf "=>>= ksubdomain需要网卡的 root 权限,请注意输入密码,或者使用 sudo 执行本脚本(推荐) \n"
 printf "=>>= 开始时间:`date` \n"
 
-res=$sub'|'$ksubdomain'|'$httpx
 
 if [ ${domain} ];then
-    printf "=>>= 开始爬取子域名...请稍等...... \n"
-    eval $res
-    find_subdomain
+    printf "=>>= 开始爬取 ${domain} 子域名...请稍等...\n"
+    eval $sub
+    sleep 1
+    printf "=>>= 开始验证 ${domain} 子域名有效性...\n"
+    eval $ksubdomain
+    sleep 1
+    printf "=>>= 开始获取 status_codes、content_length、title...\n"
+    eval $httpx
+    over
 else
     printf "\n"
     printf "=!!= 脚本已经退出,请先输入查询的域名.\n"
